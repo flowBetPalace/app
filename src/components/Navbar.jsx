23d9e0c97ca29d4aed37482db13ab2ff342a2ae5
InigoZepeda <inigis@hotmail.com>
2023-06-29 01:00:35 -0600

    More navbar styling
    35% navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,43 @@ import styles from '@/assets/styles/Navbar.module.css'
 
 export default function Navbar() {
     return(
-        <nav className={styles.nav}>
-            <p>Hola</p>
-
+        <nav className={styles.navbar}>
+            <div className={styles.navbarContainer + 'container'}>
+                <div className={styles.content}>
+                    <div className={styles.logo}>
+                        <Link href="/" className={styles.logoImg}>
+                            <Image
+                                src='/imgs/flowbetpalace-logo.svg'
+                                alt='FlowBetPalace logo'
+                                width={180}
+                                height={37}
+                                priority 
+                            />
+                        </Link>
+                    </div>
+                    <div className={styles.center + ' navlinks'}>
+                        <ul className={styles.links}>
+                            <li>
+                                <Link href="/sports" className={styles.navLink}>
+                                    Sports
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/events" className={styles.navLink}>
+                                    Events
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.right}>
+                        <div className={styles.connectBtn}>
+                            <button>
+                                Log in
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
