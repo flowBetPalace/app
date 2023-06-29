b5382dcff50c9e415efdc6ba63cda6c91ee5c553
InigoZepeda <inigis@hotmail.com>
2023-06-29 11:45:56 -0600

    Connect Wallet Finished 100%
    Finished connect wallet with connection on DataContext, connect on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,64 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/assets/styles/Navbar.module.css'
 
+import * as fcl from "@onflow/fcl";
+
 export default function Navbar() {
+
+    const formatAddress = (address) => {
+        if (address?.length === 18) {
+          return address.substring(0, 3) + "..." + address.substring(16);
+        } else {
+          return address;
+        }
+    }
+
+    const { user, setUser } = useContext(DataContext);
+
+    useEffect(() => fcl.currentUser.subscribe(setUser), [])
+
+    const AuthedState = () => {
+        return (
+          <div className={styles.connectBtn}>
+            <button>
+                {(formatAddress(user?.addr))}
+                <Image
+                    src="/icons/chevron.svg"
+                    alt="Chevron icon"
+                    width={20}
+                    height={20}
+                />
+            </button>
+            <div className={styles.dropdown}>
+                <Link href="#" onClick={fcl.unauthenticate} className={styles.dropdownLink}>
+                <Image
+                src="/icons/logout.svg"
+                alt="Logout icon"
+                width={30}
+                height={30}
+                />
+                Log out
+                </Link>
+            </div>
+            {/* <button onClick={fcl.unauthenticate}>Log Out</button> */}
+          </div>
+        )
+      }
+
+      const UnauthenticatedState = () => {
+        return (
+          <div className={styles.connectBtn}>
+            <button onClick={fcl.logIn}>Log In</button>
+            {/* <button onClick={fcl.signUp}>Sign Up</button> */}
+          </div>
+        )
+      }
+
+
+
     return(
         <nav className={styles.navbar}>
-            <div className={styles.navbarContainer + 'container'}>
+            <div className={styles.navbarContainer + ' container'}>
                 <div className={styles.content}>
                     <div className={styles.logo}>
                         <Link href="/" className={styles.logoImg}>
@@ -22,8 +76,8 @@ export default function Navbar() {
                             />
                         </Link>
                     </div>
-                    <div className={styles.center + ' navlinks'}>
-                        <ul className={styles.links}>
+                    <div className={styles.center}>
+                        <ul>
                             <li>
                                 <Link href="/sports" className={styles.navLink}>
                                     Sports
@@ -37,11 +91,14 @@ export default function Navbar() {
                         </ul>
                     </div>
                     <div className={styles.right}>
-                        <div className={styles.connectBtn}>
+                        {user.loggedIn ? <AuthedState />
+                        : <UnauthenticatedState />
+                        }
+                        {/* <div className={styles.connectBtn}>
                             <button>
                                 Log in
                             </button>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -2,23 +2,25 @@
 
 import { createContext, useEffect, useState, useContext } from "react";
 import * as fcl from "@onflow/fcl";
+import "../../flow/config";
 
 export const DataContext = createContext(null);
 
 export const DataProvider = ({children}) => {
-    const [userWallet, setUserWallet] = useState(null);
+    // const [userWallet, setUserWallet] = useState(null);
+    const [user, setUser] = useState({loggedIn: null})
 
-    useEffect(() => {
-        // Connect with Blocto using FCL
-        fcl.authenticate().then((user) => {
-        if (user.cid) {
-            setUserWallet(user);
-        }
-        });
-    }, []);
+    // useEffect(() => {
+    //     // Connect with Blocto using FCL
+    //     fcl.authenticate().then((user) => {
+    //     if (user.cid) {
+    //         setUserWallet(user);
+    //     }
+    //     });
+    // }, []);
 
     return (
-        <DataContext.Provider value={{ userWallet, setUserWallet }}>
+        <DataContext.Provider value={{ user, setUser }}>
             {children}
         </DataContext.Provider>
     );
