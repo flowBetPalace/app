34396ab28b09a2c3fde4da5de5a66ec8dc627bc7
Jazthedev <joelpepa2@gmail.com>
2023-07-04 21:22:44 +0200

    add selectedBet track

--- src/components/Widgets/ChildBet.jsx
+++ src/components/Widgets/ChildBet.jsx
@@ -1,16 +1,21 @@
 import styleBet from '@/assets/styles/StyleBet.module.css'
+import { useState } from 'react'
 
 export default function ChildBet({uuid,name,options,winnerOptionsIndex,odds,startDate,stopAcceptingBetsDate,endDate}){
+    const [selectedIndex,setSelectedIndex] = useState(null);
 
-     
+    const onClickBuyBet = () => {
+        
+    }
+
     return  <div className={styleBet.childBet}>
                         <p className={styleBet.childName}>{name}</p>
                         <div className={styleBet.childContainer}>
                             {options.map((option, optionIndex) => (
                                 <>
-                                <div key={optionIndex} className={styleBet.childSelection}>
+                                <div key={optionIndex} className={styleBet.childSelection} onClick={()=>{setSelectedIndex(optionIndex)}}>
                                     <p className={styleBet.option}>
-                                        {option}
+                                        {option} 
                                     </p>
                                     <p className={styleBet.odds}>
                                       {odds[optionIndex]}
