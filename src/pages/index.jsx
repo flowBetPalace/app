9aa82fb6691855193da2b87463728c9d678cc0a2
Esteban Zepeda <estzepeda@outlook.com>
2023-07-04 20:50:51 -0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,6 +22,52 @@ export default function Home() {
   const [name, setName] = useState('')
   const [transactionStatus, setTransactionStatus] = useState(null) // NEW
   const {categories, setCategories } = useContext(DataContext);
+  const [bets, setBets] = useState([]);
+
+  async function getBets() {
+    const response = await fcl.query({
+        cadence: `
+            import FlowBetPalace from 0x036703c904a81123
+
+            // This script gets recent added bets
+            
+            pub fun main(category: String,skip: Int) :[[String]]{
+                let amountReturnedBets = 5
+                // Get the accounts' public account objects
+                let acct1 = getAccount(0x036703c904a81123)
+            
+                // Get references to the account's receivers
+                // by getting their public capability
+                // and borrowing a reference from the capability
+                let scriptRef = acct1.getCapability(FlowBetPalace.scriptPublicPath)
+                                    .borrow<&FlowBetPalace.Script>()
+                                    ?? panic("Could not borrow acct1 vault reference")
+            
+                let bets = scriptRef.getCategoryBets(category: category,amount: amountReturnedBets,skip: skip)
+                log("bets")
+                log(bets)
+                return bets
+            }
+        
+        `,
+        args: (arg, t) => [
+            arg(currentItem, t.String),
+            arg("0", t.Int)
+        ]
+    },)
+    setBets(response)
+
+}
+useEffect(() => {
+    try{
+        getBets()
+    }catch(err){
+        console.log(err)
+    }
+
+
+
+}, [])
 
 
   useEffect(() => fcl.currentUser.subscribe(setUser), [])
@@ -139,7 +185,7 @@ export default function Home() {
             } */}
 
 
-            {matchesData.map((match, index) => (
+            {bets.map((match, index) => (
               <Match
               key={index}
               category={match.category}
