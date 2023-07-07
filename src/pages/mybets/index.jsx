87b52f55f87be605c8d1b8f4687873ac815c941e
Jazthedev <joelpepa2@gmail.com>
2023-07-07 15:41:20 +0200

    add getFinishedBets

--- src/pages/mybets/index.jsx
+++ src/pages/mybets/index.jsx
@@ -54,8 +54,37 @@ export default function myBets() {
         
     }
 
-    async function getOFinishedBets(){
+    async function getFinishedBets(){
+        if (user.loggedIn === null) {
+            return
+        }
+        try {
+            const response = await fcl.query({
+                cadence: `
+                import FlowBetPalace from 0x48214e37c07e015b
 
+
+                pub fun main(addr: Address):[FlowBetPalace.UserBetStruct] {
+                    // Get the accounts wich you want to get the bet
+                    let acct1 = getAccount(addr)
+                    //get reference of user switchboard
+                    let switcboardRef = acct1.getCapability(FlowBetPalace.userSwitchBoardPublicPath)
+                                          .borrow<&AnyResource{FlowBetPalace.UserSwitchboardPublicInterface}>()
+                                          ?? panic("User dont have any bet yet")
+                    //return old bets
+                    return switcboardRef.getFinishedBets()
+                }
+
+            
+            `,
+                args: (arg, t) => [
+                    arg(user.addr, t.Address)
+                ]
+            },)
+            console.log('res finished', response)
+            setFinishedBets(response)}catch(err){
+                console.log(err)
+            }
 
     }
 
@@ -63,6 +92,7 @@ export default function myBets() {
 
     useEffect(() => {
         getBets()
+        getFinishedBets()
     }, [user])
     return (
     <div className={styleMyBets.mybets}>
